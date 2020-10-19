de1476c0b541ae07b117bd56afc94056610c9133
hugolmn <hugo.le-moine@outlook.fr>
2020-10-19 18:26:55 +0000

    Update

--- assets/images/generate-images.sh
+++ assets/images/generate-images.sh
@@ -1,20 +1,19 @@
-mkdir "fullsize"
-for f in *.*
-	do magick convert $f -set filename:name %t fullsize/%[filename:name].jpg
-done
-
 dimensions=(1080 720 480 360 240)
 mkdir -p "${dimensions[@]}"
 
 for folder in "${dimensions[@]}"
 do
 	echo "${folder}"
-	
-	for f in *.jpg
-		do magick convert $f -set filename:name %t -resize ${folder} -quality 80 ${folder}/%[filename:name].jpg
-	done
-	
-	for f in *.JPG
+
+	for f in *.{png,PNG,jpg,JPG}
 		do magick convert $f -set filename:name %t -resize ${folder} -quality 80 ${folder}/%[filename:name].jpg
 	done
 done
+
+mkdir -p "fullsize"
+
+for f in *.{png,PNG,jpg,JPG}
+do magick convert $f -set filename:name %t fullsize/%[filename:name].jpg
+done
+rm *.{png,PNG,jpg,JPG}
+exit 0
