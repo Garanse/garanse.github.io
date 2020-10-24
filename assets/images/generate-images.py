6699625f4d8e33704f0fca60b502b9e05a33715f
hugolmn <hugo.le-moine@outlook.fr>
2020-10-24 17:23:33 +0000

    Azure pipeline automatic build

--- assets/images/generate-images.py
+++ assets/images/generate-images.py
@@ -4,19 +4,23 @@ import os
 import shutil
 
 SIZES = [1080, 720, 480, 360, 240]
+EXTENSIONS = ['.png', '.jpg', '.jpeg']
 
 for folder in ['fullsize', *map(str, SIZES)]:
     if not os.path.exists(folder):
         os.makedirs(folder)
         
-image_list = glob.glob('*.jpg') + glob.glob('*.jpeg') + glob.glob('*.png')
+image_list = [f for f in glob.glob('*') if os.path.splitext(f)[1].lower() in EXTENSIONS]
 
 for image in image_list:
-    image_name = os.path.basename(image)
+    image_name = os.path.basename(image).split('.')[0]
     with Image.open(image) as im:
+        # Change format of fullsize image
+        im.save(image_name + '.jpg', format='JPEG')
+        # Create resized versions
         for size in SIZES:
-            im.thumbnail(size=(size, size))
-            im.save(os.path.join(str(size), image_name), format='JPEG')
-    shutil.move(image, os.path.join('fullsize', os.path.basename(image)))
-    print(f'Resized: {os.path.basename(image)}')
-    
+            resized_im = im.resize((size, int(im.height/im.width * size)))
+            resized_im.save(os.path.join(str(size), image_name + '.jpg'), format='JPEG')
+    # Move original file into fullsize folder
+    shutil.move(image_name + '.jpg', os.path.join('fullsize', image_name + '.jpg'))
+    print(f'Resized: {image_name}')
